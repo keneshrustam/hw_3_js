958fff3692dc4072c771e4f6c3a2a2b0a6ef8622
keneshrustam <keneshrustam@gmail.com>
2023-10-16 21:05:47 +0600

    lol

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log("task1")
+const obj = {
+    name:"rustam",
+    age: 18,
+    city: "bishkek"
+ }
+
+
+ console.log(obj);
+
+ console.log("task2");
+
+obj['age'] = 984
+
+console.log(obj);
+
+console.log("task3")
+const numbers = [1,2,3,4,5,6]
+console.log(numbers.length)
+
+console.log("task4")
+
+const animals = []
+animals[0] = 'mishka'
+console.log(animals);
+
+console.log("task5")
+const people = [{name:"rustamm", age:18,city:"bishkek" }, {name:"naryto", age:24,city:"konoha"}, {name:"gaara", age:25,city:"hidden sand"}]
+console.log(people)
+
+console.log("task6");
+const data = ['gf',43,Boolean,Object,BigInt,undefined,null,[[]]]
+console.log(data);
+
+console.log("task7")
+const fruits = ['Абрикос','айва','апельсин','арбуз']
+console.log(fruits[fruits.length - 2]);
+
+console.log("task8")
+const book = {title: 'gari poper', author:"tatiana leanidovna", year: "do hashey eri 2m let" }
+console.log(book.author);
+
